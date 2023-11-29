fc47fca4e18e3c96e3bbccb5575a2febeaa58361
Klaudia-Czerska <klaudiaczerska9@gmail.com>
2023-11-29 19:34:21 +0100

    added cors for testing on local host

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('./db/tasks')
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
